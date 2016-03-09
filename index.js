c97c7d649b3533df68576ff7512c93954c6991cd
Kristian Epps <kristian.epps@bbc.co.uk>
2016-03-09 10:50:02 +0000

    Added post endpoint

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var fs = require('fs');
+var dataStore = [];
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+app.post('/json', function(req, res) {
+	var file = req.body;
+	dataStore.push(file);
+	res.status(200).json(file);
+});
 
 module.exports = app;
