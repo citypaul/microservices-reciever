725ffdec9fc0bd274da86a8f8b17f57e4a920e06
Kristian Epps <kristian.epps@bbc.co.uk>
2016-03-09 14:35:16 +0000

    done it

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var fs = require('fs');
 var dataStore = [];
 var Client = require('node-rest-client').Client;
 var id = 0;
+var amqp = require('amqplib/callback_api');
 
 function pushToConsumer(data) {
 	var client = new Client();
@@ -22,17 +23,41 @@ function pushToConsumer(data) {
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.post('/json', function(req, res) {
-	var data = {
-		timestamp: Date.now(),
-		id: id +=1,
-		content: req.body
-	};
-	dataStore.push(data);
-	console.log(dataStore);
-	pushToConsumer(data);
+// app.post('/json', function(req, res) {
+// 	var data = {
+// 		timestamp: Date.now(),
+// 		id: id +=1,
+// 		content: req.body
+// 	};
+// 	dataStore.push(data);
+// 	console.log(dataStore);
+// 	pushToConsumer(data);
 
-	res.status(200).json(req.body);
+// 	res.status(200).json(req.body);
+// });
+
+amqp.connect('amqp://localhost', function(err, conn) {
+  	conn.createChannel(function(err, ch) {
+    	var q = 'queue';
+
+    	ch.assertQueue(q, {durable: false});
+    	console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
+		ch.consume(q, function(message) {
+			var payload = JSON.parse(message.content.toString());
+			var data = {
+				timestamp: Date.now(),
+				id: id +=1,
+				content: payload
+			};
+
+			dataStore.push(data)
+			pushToConsumer(data);
+
+  			console.log(" [x] Received %s", JSON.stringify(payload));
+		}, {noAck: true});
+  	});
 });
+
+
 
 module.exports = app;
